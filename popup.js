291a0ec2d437f80ec6ee958a65b1a0ee8433639d
Stephanie Trinh <stephanie_trinh@berkeley.edu>
2021-01-13 19:45:49 -0800

    Added necessary files for chrome extension.

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var translateButton = document.getElementById('translatePage');
+    translateButton.addEventListener('click', function() {
+        // TODO: code to grab audio and create .wav file to pass into speech to text converter
+    });
+});
